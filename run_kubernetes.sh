ed9b6626d1bb13a58007c3c16860c0f7520a5444
Aiswaryasekar <50578292+Aiswaryasekar@users.noreply.github.com>
2021-04-14 13:52:53 +0530

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,11 +9,11 @@ dockerpath=aiswaryasekar/ml_microservices
 # Step 2
 # Run the Docker Hub container with kubernetes
 
-kubectl run mcrspredict --port=8000 --image=$dockerpath
+kubectl run mcrspredict --port=9000 --image=$dockerpath
 # Step 3:
 # List kubernetes pods
 kubectl get pods
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward pods/mcrspredict 8000:8000
+kubectl port-forward pods/mcrspredict 9000:8000
 
